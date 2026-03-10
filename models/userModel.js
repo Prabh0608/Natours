bdc932eafeff2581f247ce406e8b0fe155c9dd40
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-10 19:05:22 +0530

    basic user auth

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email!'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provid a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Please enter the same password',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next;
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next;
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
