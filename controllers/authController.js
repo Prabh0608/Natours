e21eab5e7170556eb8feba5fab2942cf67cd94fc
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-12 17:27:53 +0530

    Authenticating as admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,3 +79,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = freshUser;
   next();
 });
+
+exports.restrict = (...roles) => {
+  return (req, res, next) => {
+    console.log(req.user);
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+    next();
+  };
+};
