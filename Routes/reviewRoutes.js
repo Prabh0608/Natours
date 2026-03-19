b1f34cc2f4fe35c674d18759b8ca8ebee95faddd
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-19 19:24:04 +0530

    added data and auth

--- Routes/reviewRoutes.js
+++ Routes/reviewRoutes.js
@@ -3,11 +3,12 @@ const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrict('user'),
     reviewController.setToursUserId,
     reviewController.createReview,
@@ -16,7 +17,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .delete(reviewController.deleteReview)
-  .patch(reviewController.updateReview);
+  .delete(
+    authController.restrict('user', 'admin'),
+    reviewController.deleteReview,
+  )
+  .patch(
+    authController.restrict('user', 'admin'),
+    reviewController.updateReview,
+  );
 
 module.exports = router;
