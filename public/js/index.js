a4317b2527873f70de7a360b24bbc23e3cfef1f7
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-29 16:37:39 +0530

    logged out users

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 import '@babel/polyfill';
-import { login } from './login';
+import { login, logout } from './login';
 
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
@@ -11,3 +12,5 @@ if (loginForm) {
     login(email, password);
   });
 }
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
