3766bba830f5ee3c137f7b55ac1be4d085d2f7ad
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-29 16:08:05 +0530

    organised files and login page completed

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,6 @@
-const login = async (email, password) => {
+import axios from 'axios';
+import { showAlert } from './alert';
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -10,20 +12,12 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'Success') {
-      alert('Logged in successfully');
+      showAlert('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  console.log(email, password);
-  login(email, password);
-});
