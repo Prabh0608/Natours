27546cfb3f70252f3c9fefd46c9fd3626a51a437
Prabhjot <sainiprabhjot75@gmail.com>
2026-03-30 21:43:43 +0530

    created usable mail function

--- utils/email.js
+++ utils/email.js
@@ -1,21 +1,49 @@
 const nodemailer = require('nodemailer');
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: 'd785e7d4cb628e',
-      pass: '3de8b8ecf78b39',
-    },
-  });
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-  const mailOptions = {
-    from: 'Prabhjot Singh Saini <sainiprabhjot75@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+module.exports = class Email {
+  constructor(user, url) {
+    ((this.to = user.email),
+      (this.firstName = user.name.split(' ')[0]),
+      (this.url = url),
+      (this.from = `Prabhjot Singh Saini ${process.env.EMAIL_FROM}`));
+  }
 
-  await transporter.sendMail(mailOptions);
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: 'sandbox.smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'd785e7d4cb628e',
+        pass: '3de8b8ecf78b39',
+      },
+    });
+  }
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template.pug}`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromstring(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family');
+  }
 };
-module.exports = sendEmail;
