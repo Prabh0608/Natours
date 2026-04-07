2b4038e392cd16cd92d77930ab8e4fc3f7fece3a
Prabhjot <sainiprabhjot75@gmail.com>
2026-04-07 21:40:51 +0530

    completed the bookings

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview,
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { showAlert } from './alert';
-const stripe = Stripe(
+const stripe = require('stripe')(
   'pk_test_51THOtVIfTD7A7kHjREk6XSlR4LJk8caE1BmZqbADK5OuvSjkqcorJj7wVgBEo4IBN84k0wdKN9jH378T2jtTO7Xb00LcgUAeXt',
 );
 export const bookTour = async (tourId) => {
