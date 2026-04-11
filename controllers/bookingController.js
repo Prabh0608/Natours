c95ec921e7a6fb7fe3cc087876e4f916e60e2ea1
Prabhjot <sainiprabhjot75@gmail.com>
2026-04-11 14:01:20 +0530

    debugging

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,7 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].unit_amount / 100;
+  const price = session.line_items[0].unit_amount / 100;
   console.log(tour);
   console.log(user);
   console.log(price);
@@ -67,8 +67,9 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.completed')
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
+  }
 
   res.status(200).json({ received: true });
 };
