f6a7c90c90e8e026116aba6c95acb498526a233b
Prabhjot <sainiprabhjot75@gmail.com>
2026-04-11 14:19:15 +0530

    final commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,12 +46,22 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
-  console.log(tour);
-  console.log(user);
-  console.log(price);
-  await Booking.create({ tour, user, price });
+  const userDoc = await User.findOne({
+    email: session.customer_email,
+  });
+  if (!userDoc) {
+    console.log('User not found');
+    return;
+  }
+  const stripeSession = await stripe.checkout.sessions.retrieve(session.id, {
+    expand: ['line_items'],
+  });
+  const price = stripeSession.line_items.data[0].price_data.unit_amount / 100;
+  await Booking.create({
+    tour,
+    user: userDoc.id,
+    price,
+  });
 };
 
 exports.webhookCheckout = (req, res, next) => {
