5e21872b49a50365afcb262955bad4490235a12a
Prabhjot <sainiprabhjot75@gmail.com>
2026-04-11 14:25:10 +0530

    fixing image url

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,7 +21,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`${req.protocol}://${req.get('host')}/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ],
           },
           unit_amount: tour.price * 100,
         },
